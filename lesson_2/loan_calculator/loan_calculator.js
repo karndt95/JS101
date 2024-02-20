93e2b21b8360a2a8b3e1d245da2d83e9321aab9e
Karl Arndt <karndt1995@gmail.com>
2024-02-19 19:02:05 -0500

    Add USD number formatter

--- lesson_2/loan_calculator/loan_calculator.js
+++ lesson_2/loan_calculator/loan_calculator.js
@@ -7,11 +7,40 @@ let loanAmount;
 let monthlyInterestRate;
 let durationInMonths;
 let monthlyPayment;
+let annualPercentageRate;
 
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt('Welcome to Loan Calculator!');
+function invalidNumber(number) {
+  number = cleanNumber(number);
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
+function cleanNumber(number) {
+  number = number.replace('$', '');
+  number = number.replace('%', '');
+  return number;
+}
+
+prompt('Welcome to Loan Calculator!');
+prompt('Please enter loan amount:');
+
+loanAmount = READLINE.question();
+loanAmount = cleanNumber(loanAmount);
+
+while (invalidNumber(loanAmount)) {
+  prompt("Hmm... that doesn't look like a valid number. Try again");
+  loanAmount = READLINE.question();
+}
+
+prompt('Initial loan amount = ' + USD.format(loanAmount));
+prompt('What is the annual percentage rate? Type in percentage format (ex. 5%)');
+annualPercentageRate = cleanNumber(READLINE.question());
+
+while(invalidNumber(annualPercentageRate)) {
+  prompt('Hmm... that does not look like a valid number. Try again');
+  annualPercentageRate = cleanNumber(READLINE.question());
+}
