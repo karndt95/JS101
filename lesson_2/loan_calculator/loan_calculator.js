ac771fa6f2239ea2059823984e58cae21428e2ee
Karl Arndt <karndt1995@gmail.com>
2024-02-19 19:38:22 -0500

    add loan calculation

--- lesson_2/loan_calculator/loan_calculator.js
+++ lesson_2/loan_calculator/loan_calculator.js
@@ -5,6 +5,7 @@ const USD = new Intl.NumberFormat('en-US', {
 });
 let loanAmount;
 let monthlyInterestRate;
+let durationInYears;
 let durationInMonths;
 let monthlyPayment;
 let annualPercentageRate;
@@ -26,21 +27,49 @@ function cleanNumber(number) {
 }
 
 prompt('Welcome to Loan Calculator!');
-prompt('Please enter loan amount:');
 
-loanAmount = READLINE.question();
-loanAmount = cleanNumber(loanAmount);
+while (true) {
+  prompt('Please enter loan amount:');
 
-while (invalidNumber(loanAmount)) {
-  prompt("Hmm... that doesn't look like a valid number. Try again");
   loanAmount = READLINE.question();
-}
+  loanAmount = cleanNumber(loanAmount);
 
-prompt('Initial loan amount = ' + USD.format(loanAmount));
-prompt('What is the annual percentage rate? Type in percentage format (ex. 5%)');
-annualPercentageRate = cleanNumber(READLINE.question());
+  while (invalidNumber(loanAmount)) {
+    prompt("Hmm... that doesn't look like a valid number. Try again");
+    loanAmount = READLINE.question();
+  }
 
-while(invalidNumber(annualPercentageRate)) {
-  prompt('Hmm... that does not look like a valid number. Try again');
+  prompt('Initial loan amount = ' + USD.format(loanAmount));
+  prompt('What is the annual percentage rate? Type in percentage format (ex. 5%)');
   annualPercentageRate = cleanNumber(READLINE.question());
+
+  while (invalidNumber(annualPercentageRate)) {
+    prompt('Hmm... that does not look like a valid number. Try again');
+    annualPercentageRate = cleanNumber(READLINE.question());
+  }
+  monthlyInterestRate = Number(annualPercentageRate / 100) / 12;
+
+  prompt('Enter loan duration in years');
+  durationInYears = READLINE.question();
+
+  while (invalidNumber(durationInYears)) {
+    prompt("Hmm... that does not look like  a valid number. Try again");
+    durationInYears = READLINE.question();
+  }
+  durationInMonths = Number(durationInYears) * 12;
+
+  monthlyPayment = Number(loanAmount) *
+    (monthlyInterestRate /
+      (1 - Math.pow((1 + monthlyInterestRate), (-durationInMonths))));
+
+  prompt(`Your monthly payment is ${USD.format(monthlyPayment)}`);
+
+  prompt('Another calculation?');
+  let answer = READLINE.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = READLINE.question().toLowerCase();
+  }
+
+  if (answer[0] === 'n') break;
 }
