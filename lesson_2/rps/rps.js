62f83108f2b499f89b63ff73083506e7de2dd4d3
Karl Arndt <karndt1995@gmail.com>
2024-02-23 08:42:01 -0500

    make displayWinner() more readable and create restoreToOriginal() function to allow user to type first letter for choice

--- lesson_2/rps/rps.js
+++ lesson_2/rps/rps.js
@@ -6,20 +6,32 @@ function prompt(message) {
 }
 
 function displayWinner(choice, computerChoice) {
-  if ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-    (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-    (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-    (choice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
-    (choice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'rock' && computerChoice === 'lizard') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'paper' && computerChoice === 'spock') ||
+      (choice === 'scissors' && computerChoice === 'paper') ||
+      (choice === 'scissors' && computerChoice === 'lizard') ||
+      (choice === 'lizard' && computerChoice === 'paper') ||
+      (choice === 'lizard' && computerChoice === 'spock') ||
+      (choice === 'spock' && computerChoice === 'rock') ||
+      (choice === 'spock' && computerChoice === 'scissors')) {
     prompt('You win!');
-  } else if ((choice === 'rock' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
-    (choice === 'paper' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-    (choice === 'scissors' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-    (choice === 'lizard' && (computerChoice === 'rock' || computerChoice === 'scissors')) ||
-    (choice === 'spock' && (computerChoice === 'paper' || computerChoice === 'lizard'))) {
-    prompt('Computer wins!');
+  } else if (choice === computerChoice) {
+    prompt("It's a tie!");
   } else {
-    prompt("It's a tie");
+    prompt("Computer wins!");
+  }
+}
+
+function restoreToOrginalString(string) {
+  switch (string) {
+    case 'r': return 'rock';
+    case 'p': return 'paper';
+    case 'l': return 'lizard';
+    case 'sp' : return 'spock';
+    case 'sc' : return 'scissors';
+    default: return string;
   }
 }
 
@@ -27,9 +39,16 @@ while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
+  if (choice.length <= 2) {
+    choice = restoreToOrginalString(choice);
+  }
+
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
     choice = readline.question();
+    if (choice.length <= 2) {
+      choice = restoreToOrginalString(choice);
+    }
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
