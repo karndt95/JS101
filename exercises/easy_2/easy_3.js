5c8e36ddf8f8d55ce86fadf88ccadb0eef6a4982
xRamyun <xramyuntft@yahoo.com>
2024-02-27 09:10:02 -0500

    Add question 3

---
+++ exercises/easy_2/easy_3.js
@@ -0,0 +1,16 @@
+/*
+Multiplying Two Numbers
+
+Create a function that takes two arguments, 
+multiplies them together, and returns the result.
+
+Example:
+
+console.log(multiply(5, 3) === 15); // logs true
+*/
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+console.log(multiply(5, 3) === 15); // logs true
