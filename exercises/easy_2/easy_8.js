9a12f5d34cdf952cc84cdaed0735a2a98d3560d5
Karl Arndt <karndt1995@gmail.com>
2024-02-28 03:44:58 -0500

    Add question 8

---
+++ exercises/easy_2/easy_8.js
@@ -0,0 +1,30 @@
+/*
+Odd Lists
+
+Write a function that returns an Array that contains every other element of an Array that is passed in as an argument. 
+The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+
+Examples:
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
+*/
+
+function oddities(arr) {
+  let oddArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (index % 2 === 0) {
+      oddArr.push(arr[index]);
+    }
+  }
+  return oddArr;
+}
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
