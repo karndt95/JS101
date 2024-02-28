14116511a51933bd765d9a313229b89efe4fa853
Karl Arndt <karndt1995@gmail.com>
2024-02-28 03:33:58 -0500

    Add question 6

--- exercises/easy_2/easy_6.js
+++ exercises/easy_2/easy_6.js
@@ -19,3 +19,6 @@ function penultimate(words) {
   arr = words.split(' ');
   return arr[arr.length - 2];
 }
+
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is great!") === "is"); // logs true
