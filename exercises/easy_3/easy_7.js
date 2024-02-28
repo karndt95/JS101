204c7ef2eadb2515cb148bdb6ca82e7692a7dcfa
Karl Arndt <karndt1995@gmail.com>
2024-02-28 05:49:06 -0500

    Add question 7

---
+++ exercises/easy_3/easy_7.js
@@ -0,0 +1,45 @@
+/*
+Double Doubles
+
+A double number is an even-length number whose left-side digits are exactly the same as its right-side digits. 
+For example, 44, 3333, 103103, and 7676 are all double numbers, whereas 444, 334433, and 107 are not.
+
+Write a function that returns the number provided as an argument multiplied by two, 
+unless the argument is a double number; otherwise, return the double number as-is.
+
+Examples:
+
+twice(37);          // 74
+twice(44);          // 44
+twice(334433);      // 668866
+twice(444);         // 888
+twice(107);         // 214
+twice(103103);      // 103103
+twice(3333);        // 3333
+twice(7676);        // 7676
+*/
+
+function twice(num) {
+  if (isDoubleNumber(num)) {
+    return num;
+  }
+  return num * 2;
+}
+
+function isDoubleNumber(number) {
+  let stringNumber = number.toString();
+  let center = Math.floor(stringNumber.length / 2);
+  let leftNumber = stringNumber.substring(0, center);
+  let rightNumber = stringNumber.substring(center);
+
+  return leftNumber === rightNumber;
+}
+
+console.log(twice(37));
+console.log(twice(44));
+console.log(twice(334433));
+console.log(twice(444));
+console.log(twice(107));
+console.log(twice(103103));
+console.log(twice(3333));
+console.log(twice(7676));
